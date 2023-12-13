e89b4e2c680243482587ff926a6f821ef68888a4
Sean Stetson <sstetson@wesleyan.edu>
2023-12-12 23:57:58 -0500

    Implemented skeleton

--- flaskr/GetUploadedContent.py
+++ flaskr/GetUploadedContent.py
@@ -1,18 +1,48 @@
 from collections import callable
 import os
+import json
 
 def GetDirectories(path: str) -> list[str]:
     """Gets all directories in the user-uploaded section. This is the first step,
     more logic will follow to verify the data contained"""
-    pass
+    
+    directories = []
+    for x in os.listdir(path):
+        if os.path.isdir((dir := os.join(path, x))):
+            directories.append(dir)
 
-def GetJSONFiles(directories: list[str]) -> list[dict]:
+    return directories
+
+def GetJSONFiles(directories: list[str], jsonName: str) -> list[dict]:
     """Gets all .json files associated with user uploaded scripts"""
-    pass
+    
+    jsonDicts = []
+    for dir in directories:
+        if os.path.isfile((file := os.path.join(dir, jsonName))):
+            with open(file, "r") as f:
+                jsonDicts.append(json.load(f))
 
-def CheckJSONFiles(jsonFiles: list[dict], requiredFields: list[str]) -> list[dict]:
+    return jsonDicts
+
+
+def CheckJSONFiles(jsonDicts: list[dict], requiredFields: list[str]) -> list[dict]:
     """Returns the .json dictionaries that have all of the required fields"""
 
-def VerifyFields(jsonFiles: list[dict], verify: dict[str, callable[[str], bool]]) -> list[dict]:
+    for d in jsonDicts:
+        for field in requiredFields:
+            if field not in d:
+                jsonDicts.remove(d)
+    
+    return jsonDicts
+
+
+def VerifyFields(jsonDicts: list[dict], verify: dict[str, callable[[str], bool]]) -> list[dict]:
     """Returns the .json dictionaries that pass some sort of verification on a specific field"""
+
+    for d in jsonDicts:
+        for key, check in verify.items():
+            if not check(d[key]):
+                jsonDicts.remove(d)
+
+    return jsonDicts
 
